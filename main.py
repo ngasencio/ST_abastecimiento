6fe8c2da6bdcfb5eb46129db4b7f7e9a31ece053
ngasencio <ng.asencio@gmail.com>
2026-02-20 12:59:46 -0300

    feat: Correo LI

--- main.py
+++ main.py
@@ -28,9 +28,7 @@ pagina_ejecucionpac2 = st.Page("pages/dash_ejecucionpac2.py", title="Ejecución 
 pagina_ordenes = st.Page("pages/dash_ordencompra.py", title="Ordenes de Compra", icon="🧾")
 pagina_compraagil = st.Page("pages/dash_compraagil.py", title="Compra Ágil", icon="⚡")
 pagina_licitaciones = st.Page("pages/dash_licitaciones.py", title="Licitaciones", icon="📄")
-pagina_licitaciones2 = st.Page("pages/dash_licitaciones2.py", title="Licitaciones2", icon="📄")
-
-
+pagina_licitaciones2 = st.Page("pages/dash_licitaciones2.py", title="Licitaciones", icon="📄")
 
 #Grupo: Documentos
 pagina_documentos = st.Page("pages/dash_documentos.py", title="Documentos", icon="📋")
