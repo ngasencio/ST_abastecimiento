e6d11d5c5820ac6b673b06b69bfa62b342109147
ngasencio <ng.asencio@gmail.com>
2026-03-03 08:47:33 -0300

    Cambios Previos

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ pagina_buscadorOC = st.Page("pages/tools_buscadorOC.py", title="Buscador OC", ic
 
 #Grupo: Finanzas
 pagina_anexo1 = st.Page("pages/dash_anexo1.py", title="Anexo N°1", icon="🧮")
-pagina_anexo2 = st.Page("pages/dash_anexo2.py", title="Anexo N°2 (DEBUG)", icon="🧮")
+pagina_anexo2 = st.Page("pages/dash_anexo2.py", title="Anexo N°2 D", icon="🧮")
 pagina_anexo3 = st.Page("pages/dash_anexo3.py", title="Anexo N°3", icon="🧮")
 pagina_anexo4 = st.Page("pages/dash_anexo4.py", title="Anexo N°4", icon="🧮")
 
